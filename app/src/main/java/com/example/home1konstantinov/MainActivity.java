82dfe9bf59e8df01548ca00322c2f83cf45ea933
Nickolay Konstantinov <konst007@tut.by>
2019-02-01 00:15:50 +0300

    feature: fabric

--- app/src/main/java/com/example/home1konstantinov/MainActivity.java
+++ app/src/main/java/com/example/home1konstantinov/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.home1konstantinov;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.answers.Answers;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Answers());
         setContentView(R.layout.activity_main);
     }
 }
